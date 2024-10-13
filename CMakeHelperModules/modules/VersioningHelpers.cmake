e55fd3d9b983ca32fc91227796094fde6d49889a
Benno Doerr <bencayd@gmail.com>
2024-10-13 10:09:32 +0200

    Fix VCS detection
    * Now works from all subdirectories

--- CMakeHelperModules/modules/VersioningHelpers.cmake
+++ CMakeHelperModules/modules/VersioningHelpers.cmake
@@ -1,24 +1,40 @@
 function(getPatchVersion OutputVariable)
-    # Subversion
-    if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.svn")
-        MESSAGE(STATUS "Subversion found")
+    # Determine the VCS used
+    execute_process(
+            COMMAND git rev-parse --is-inside-work-tree
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE IsGitRepo
+            OUTPUT_QUIET
+            ERROR_QUIET
+    )
+    execute_process(
+            COMMAND svn info --show-item revision
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE IsSvnRepo
+            OUTPUT_QUIET
+            ERROR_QUIET
+    )
+
+    # Set the patch version
+    if (IsSvnRepo EQUAL "0")
+        MESSAGE(DEBUG "Subversion repository found")
         execute_process(
                 COMMAND svn info --show-item revision
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                 OUTPUT_VARIABLE ${OutputVariable}
                 OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
-        # Git
-    elseif (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
-        MESSAGE(STATUS "Git found")
+    elseif (IsGitRepo EQUAL "0")
+        MESSAGE(DEBUG "Git repository found")
         execute_process(
                 COMMAND git log -1 --format=%H ${CMAKE_CURRENT_SOURCE_DIR}
                 COMMAND xargs git rev-list --count
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                 OUTPUT_VARIABLE ${OutputVariable}
                 OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
-        # Not supported
     else ()
         MESSAGE(WARNING "Can't determine patch version. VCS not found / supported.")
         set(${OutputVariable} "0")
