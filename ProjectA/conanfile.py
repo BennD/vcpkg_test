db717d3b69aa85059e3ad2ecacac8f6e21b1f0d3
Benno Doerr <bencayd@gmail.com>
2024-10-16 20:29:47 +0200

    Add `shared` library option to conan recipe

--- ProjectA/conanfile.py
+++ ProjectA/conanfile.py
@@ -5,6 +5,8 @@ from conan.tools.cmake import CMakeToolchain
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
 
     def requirements(self):
         self.requires("spdlog/1.14.1")
